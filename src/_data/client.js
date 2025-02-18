9491d50fd784744e5a888b43ec2965a4194e46a4
Adam Moldovanyi <80765892+AdamMoldovanyi@users.noreply.github.com>
2025-02-17 20:35:02 -0500

    Initial Commit

---
+++ src/_data/client.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "name": "Gardners Digital Solutions",
+    "email": "adam@oxideconsulting.com",
+    "phoneForTel": "717-818-1625",
+    "phoneFormatted": "(717) 818-1625",
+    "address": {
+        "lineOne": "First Address Line",
+        "lineTwo": "Second Address Line",
+        "city": "Carlisle",
+        "state": "PA",
+        "zip": "80206",
+        "country": "US",
+        "mapLink": "https://maps.app.goo.gl"
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/",
+        "instagram": "https://www.instagram.com/",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://www.gardnersdigital.com"
+};
